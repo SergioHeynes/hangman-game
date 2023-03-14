4ffb6b27bab94e27df49ec58911deeed7ca9940b
Sergio Heynes <sergioheynes@machine.local>
2023-03-13 17:04:07 -0700

    Printed game status. Initialized gessed letters.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+	"unicode"
 )
 
 var dictionary = []string{
@@ -17,18 +18,52 @@ var dictionary = []string{
 	"Mural",
 	"Abecedario",
 	"Sistema solar",
+	"Estados Unidos de América",
 }
 
 func main() {
 
 	rand.Seed(time.Now().UnixNano())
 
-	tagetWord := randomWord()
-	fmt.Println(tagetWord)
+	targetWoerd := randomWord()
 
+	gessedLetters := initializeGuessedWords(targetWoerd)
+	printGameState(targetWoerd, gessedLetters)
+
+	// fmt.Println(tagetWord)
+
+}
+
+func initializeGuessedWords(targetWoerd string) map[rune]bool {
+	gessedLetters := map[rune]bool{}
+	gessedLetters[unicode.ToLower(rune(targetWoerd[0]))] = true
+	gessedLetters[unicode.ToLower(rune(targetWoerd[len(targetWoerd)-1]))] = true
+
+	return gessedLetters
 }
 
 func randomWord() string {
 	tagetWord := dictionary[rand.Intn(len(dictionary))]
 	return tagetWord
+}
+
+func printGameState(targetWord string, gessedLetters map[rune]bool) {
+
+	for _, ch := range targetWord {
+
+		if ch == ' ' {
+			fmt.Print(" ")
+		} else if gessedLetters[unicode.ToLower(ch)] {
+
+			fmt.Print(string(ch))
+
+		} else {
+			fmt.Print("_")
+		}
+
+		fmt.Print(" ")
+
+	}
+	fmt.Println()
+
 }
